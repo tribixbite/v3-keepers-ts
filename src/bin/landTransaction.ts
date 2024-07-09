07292de732175beefd71e677ed8f810cc2320a9e
Will <willstone@gmail.com>
2024-07-08 23:13:04 -0400

    feat: support VersionedTx, create LUT, keys priorfee &fix, logging, new retry&confirm logic

--- src/bin/landTransaction.ts
+++ src/bin/landTransaction.ts
@@ -26,20 +26,30 @@ import { getSimulationComputeUnits } from "@solana-developers/helpers";
 import {
   AddressLookupTableAccount,
   Connection,
+  TransactionInstruction,
   VersionedTransaction,
   Transaction as Web3Transaction,
 } from "@solana/web3.js";
-import bs58 from "bs58";
+import { encode, decode } from "bs58";
 
-// https://www.helius.dev/blog/how-to-land-transactions-on-solana
-const MAX_SECONDS_TO_SEND_TRANSACTION = 110; // s
 const MAX_RETRIES = 50; // count
 const MAX_LOOPS = 5; // count
 const RETRY_DELAY = 100; // ms
-const TX_RETRY_INTERVAL = 1000; // ms
+const TX_RETRY_INTERVAL = 1400; // ms
+const MAX_SECONDS_TO_SEND_TRANSACTION = 110; // s
+// https://www.helius.dev/blog/how-to-land-transactions-on-solana
+// https://docs.triton.one/chains/solana/sending-txs
+// https://github.com/rpcpool/optimized-txs-examples/blob/main/jupiterSwap.mjs
+
+type TransactionInput = Web3Transaction | VersionedTransaction | TransactionInstruction[];
+
+//  const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+//  const getExponentialBackoff = (attempt: number, baseDelay: number = INITIAL_RETRY_DELAY) =>
+//    Math.min(baseDelay * Math.pow(2, attempt), 10000);
 
 export async function sendAndConfirmTransactionOptimized(
-  transaction: Web3Transaction | VersionedTransaction,
+  transaction: TransactionInput,
   privateKey?: string | Uint8Array,
   lookupTables: UmiPublicKey[] = []
   //   signer?: string | Uint8Array
@@ -55,7 +65,7 @@ export async function sendAndConfirmTransactionOptimized(
   const umi = createUmi(rpc).use(mplToolbox());
 
   // check if keypair is string, if so convert to Uint8Array
-  const feePayerArr = typeof feePayer === "string" ? bs58.decode(feePayer) : feePayer;
+  const feePayerArr = typeof feePayer === "string" ? decode(feePayer) : feePayer;
   const umiKeypair = umi.eddsa.createKeypairFromSecretKey(feePayerArr);
   const umiSigner = createSignerFromKeypair({ eddsa: umi.eddsa }, umiKeypair);
   umi.use(signerIdentity(umiSigner));
@@ -78,35 +88,56 @@ export async function sendAndConfirmTransactionOptimized(
             programId: publicKey(ix.programId),
             data: ix.data,
           }))
-        : toWeb3JsLegacyTransaction(
+        : transaction instanceof VersionedTransaction
+        ? toWeb3JsLegacyTransaction(
             umi.transactions.deserialize(transaction.serialize())
-          ).instructions.map((ix) => fromWeb3JsInstruction(ix)),
+          ).instructions.map((ix) => fromWeb3JsInstruction(ix))
+        : transaction.map((ix) => fromWeb3JsInstruction(ix)),
     payer: umiSigner.publicKey,
     blockhash: "",
   });
-
+  const serializedTxMsg = encode(umiTransaction.serializedMessage);
   const fixMeIxs =
     transaction instanceof Web3Transaction
       ? transaction.instructions.map((ix) => fromWeb3JsInstruction(ix))
-      : toWeb3JsLegacyTransaction(fromWeb3JsTransaction(transaction)).instructions.map((ix) =>
+      : transaction instanceof VersionedTransaction
+      ? toWeb3JsLegacyTransaction(fromWeb3JsTransaction(transaction)).instructions.map((ix) =>
           fromWeb3JsInstruction(ix)
-        );
+        )
+      : transaction.map((ix) => fromWeb3JsInstruction(ix));
   const wrappedIxs = fixMeIxs.map((ix) => ({
     instruction: ix,
     signers: [umi.payer],
     bytesCreatedOnChain: 0,
   }));
 
-  const priorityFee = await getPriorityFeeEstimate(
-    heliusEndpoint,
-    bs58.encode(
-      (
-        await transactionBuilder(wrappedIxs).buildWithLatestBlockhash(umi, {
-          commitment: "confirmed",
-        })
-      ).serializedMessage
-    )
-  );
+  //   if (lookupTables.length === 0) { this works but adds time
+  //     const addresses = fixMeIxs
+  //       .map((ix) => ix.keys)
+  //       .flat()
+  //       .map((k) => k.pubkey);
+  //     console.log({ addresses });
+  //     const [lutBuilder, lut] = createLut(umi, {
+  //       recentSlot: BigInt(await umi.rpc.getSlot({ commitment: "finalized" }) - 5),
+  //       addresses,
+  //     });
+  //     await lutBuilder.sendAndConfirm(umi);
+  //     lookupTables.push(lut.publicKey);
+  //     console.log({ lut });
+  //   }
+  //   const serializedBase58Tx = encode(
+  //     (
+  //       await transactionBuilder(wrappedIxs).buildWithLatestBlockhash(umi, {
+  //         commitment: "confirmed",
+  //       })
+  //     ).serializedMessage
+  //   );
+  const accountKeys = wrappedIxs
+    .flatMap((ix) => ix.instruction)
+    .flatMap((ix) => ix.keys)
+    .flatMap((a) => String(a.pubkey)); //.flatMap((key) => key.pubkey.toBase58()))
+  //   console.log({ serializedBase58Tx, heliusEndpoint });
+  const priorityFee = await getPriorityFeeEstimate(heliusEndpoint, accountKeys);
   console.log({ priorityFee });
 
   const luts = lookupTables.length > 0 ? await fetchAllAddressLookupTable(umi, lookupTables) : [];
@@ -139,18 +170,20 @@ export async function sendAndConfirmTransactionOptimized(
 
   // Construct final transaction with optimal compute unit limit
   const finalTransaction = await transactionBuilder(wrappedIxs)
-    .prepend(setComputeUnitPrice(umi, { microLamports: priorityFee }))
+    .prepend(setComputeUnitPrice(umi, { microLamports: Math.ceil(priorityFee) }))
     .prepend(setComputeUnitLimit(umi, { units: computeUnitLimit }))
     .setAddressLookupTables(luts)
     .setFeePayer(umi.payer);
-
   for (let retries = 0; retries < MAX_LOOPS; retries++) {
+    console.log("looping");
     try {
       const signature = await sendAndConfirmWithRetry(finalTransaction, umi, connection);
       if (signature) {
-        return bs58.encode(signature);
+        console.log(`Transaction confirmed: ${signature}`);
+        return signature;
       } else {
-        break;
+        console.log("Transaction failed, retrying...");
+        await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
         // throw new Error("Transaction confirmation failed");
       }
     } catch (error) {
@@ -158,7 +191,7 @@ export async function sendAndConfirmTransactionOptimized(
       if (retries === MAX_RETRIES - 1) {
         throw new Error(`Failed to send transaction after ${MAX_RETRIES} attempts`);
       }
-      await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+      break;
     }
   }
 
@@ -169,7 +202,7 @@ async function sendAndConfirmWithRetry(
   finalTx: TransactionBuilder,
   umi: Umi,
   connection: Connection
-): Promise<Uint8Array | null> {
+): Promise<string | null> {
   // record the time and response time of getLatestBlockhashAndContext:
   const startTime = Date.now();
   console.time(`${new Date(startTime).toISOString()}: Total transaction time`);
@@ -194,8 +227,18 @@ async function sendAndConfirmWithRetry(
   const signedTx = await finalTx.setBlockhash(blockhash).buildAndSign(umi);
 
   for (let attempt = 0; attempt < MAX_RETRIES; attempt++) {
+    console.log(`${new Date().toISOString()} Attempt ${attempt + 1}/${MAX_RETRIES}`);
+    if (confirmedTx) {
+      console.log(`${new Date().toISOString()} Transaction confirmed`);
+      break;
+    }
     if ((Date.now() - startTime) / 1000 > MAX_SECONDS_TO_SEND_TRANSACTION) {
+      console.error(`${new Date().toISOString()} Transaction took too long to send`);
       throw new Error("Transaction took too long to send");
+    }
+    if ((await connection.getBlockHeight()) > lastValidBlockHeight) {
+      console.log(`${new Date().toISOString()} Blockhash expired, retrying...`);
+      break;
     }
     try {
       txSignature = signedTx.signatures[0];
@@ -204,14 +247,14 @@ async function sendAndConfirmWithRetry(
       console.log(`${new Date().toISOString()} Subscribing to transaction confirmation`);
 
       // confirmTransaction throws error, handle it
-      confirmTransactionPromise = await umi.rpc.confirmTransaction(txSignature, {
+      confirmTransactionPromise = umi.rpc.confirmTransaction(txSignature, {
         strategy: { type: "blockhash", blockhash, lastValidBlockHeight },
         commitment: "confirmed",
         minContextSlot: minContextSlot,
       });
 
-      console.log(`${new Date().toISOString()} Sending Transaction ${txSignature}`);
-      let sig = await umi.rpc.sendTransaction(signedTx, {
+      console.log(`${new Date().toISOString()} Sending Transaction ${encode(txSignature)}`);
+      await umi.rpc.sendTransaction(signedTx, {
         skipPreflight: true,
         commitment: "confirmed",
         maxRetries: 0,
@@ -238,29 +281,30 @@ async function sendAndConfirmWithRetry(
             TX_RETRY_INTERVAL * txSendAttempts++
           }ms, resending`
         );
-        sig = await umi.rpc.sendTransaction(signedTx, {
+        await umi.rpc.sendTransaction(signedTx, {
           skipPreflight: true,
           commitment: "confirmed",
           maxRetries: 0,
           minContextSlot,
           preflightCommitment: "confirmed",
         });
-        console.log({ sig });
       }
     } catch (error) {
       console.error(`${new Date().toISOString()} Error: ${error}`);
       console.log(`Send transaction failed, retrying (${attempt + 1}/${MAX_RETRIES})...`);
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
-    if (!confirmedTx) {
+    if (!confirmedTx || !txSignature) {
       console.log(`${new Date().toISOString()} Transaction failed`);
       return null;
     }
     console.log(`${new Date().toISOString()} Transaction successful`);
     console.log(
-      `${new Date().toISOString()} Explorer URL: https://explorer.solana.com/tx/${txSignature}`
+      `${new Date().toISOString()} Explorer URL: https://explorer.solana.com/tx/${encode(
+        txSignature
+      )}`
     );
-    return txSignature;
+    return encode(txSignature);
   }
 
   //   if (!sig) {
@@ -274,21 +318,36 @@ async function sendAndConfirmWithRetry(
   return null;
 }
 
-async function getPriorityFeeEstimate(
-  heliusUrl: string,
-  serializedTransaction: string
-): Promise<number> {
-  // const connection = new Connection(heliusUrl);
-  // transaction: bs58.encode(transaction.serialize()),
-  //   const serializedTransaction = transaction.buildWithLatestBlockhash(umi, {commitment: "confirmed"}).serialize(
+async function getPriorityFeeEstimate(heliusUrl: string, accountKeys: string[]): Promise<number> {
   const response = await fetch(heliusUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      priorityLevel: "high",
-      transaction: serializedTransaction,
+      jsonrpc: "2.0",
+      id: "my-id",
+      method: "getPriorityFeeEstimate",
+      params: [
+        {
+          accountKeys,
+          //   transaction: serializedTransaction,
+          options: { includeAllPriorityFeeLevels: true },
+        },
+      ],
     }),
   });
-  const { priorityFeeEstimate } = await response.json();
-  return priorityFeeEstimate;
+  const { result } = await response.json();
+  //   console.log({ result });
+  // {
+  //     result: {
+  //       priorityFeeLevels: {
+  //         min: 0,
+  //         low: 500,
+  //         medium: 32250,
+  //         high: 269922.25,
+  //         veryHigh: 3000000,
+  //         unsafeMax: 2000000000,
+  //       },
+  //     },
+  //   }
+  return result.priorityFeeLevels?.high ?? 1;
 }
